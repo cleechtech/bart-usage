b4cd246f2e91dcc943b22767acf04d4957a30813
Connor Leech <connorleech@gmail.com>
2014-11-09 21:47:28 -0800

    add annoying welcome message

--- public/scripts/controllers/home.js
+++ public/scripts/controllers/home.js
@@ -1,6 +1,7 @@
 'use strict';
 
 app.controller('HomeCtrl', function($scope, $http) {
+	$scope.welcomeMessage = true;
     $http.get('data/bart-stops.json').then(function(res){
         $scope.BARTStops= res.data;
     });
--- public/scripts/controllers/stop.js
+++ public/scripts/controllers/stop.js
@@ -1,6 +1,7 @@
 'use strict';
 
 app.controller('StopCtrl', function($scope, $stateParams, $http, $rootScope){
+	$scope.$parent.welcomeMessage = false;
 
 	var stopName = $stateParams.stopName;
 
